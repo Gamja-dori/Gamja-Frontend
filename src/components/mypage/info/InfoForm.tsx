36300efc68e0088e9effb46253fb58667703c36e
teyeong <taetae0202@naver.com>
2024-04-06 18:22:31 +0900

    feat: connect signout to api

--- src/components/mypage/info/InfoForm.tsx
+++ src/components/mypage/info/InfoForm.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import Btn from 'components/_common/Btn';
 import WithdrawalModal from './WithdrawalModal';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { UserProfileAtom } from 'recoil/UserProfile';
 import { parsePhoneNumber } from 'components/utils/PhoneUtils';
+import { Signout } from 'api/user';
+import { useNavigate } from 'react-router-dom';
+import { SigninStateAtom } from 'recoil/Signin';
 
 const InfoForm = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
@@ -15,6 +18,14 @@ const InfoForm = () => {
     const parsed_result = parsePhoneNumber(UserProfileData.phone_number);
     setPhoneNumber(parsed_result);
   }, [UserProfileData]);
+
+  const handleSignout = async () => {
+    const res = await Signout();
+    if (res?.status === 202) {
+      window.localStorage.clear();
+      window.location.href = '/';
+    }
+  };
 
   return (
     <div className="infoForm-div">
@@ -37,7 +48,7 @@ const InfoForm = () => {
       <div className="infoForm-btn-box">
         <Btn
           label="로그아웃"
-          onClick={() => console.log('로그아웃 클릭')}
+          onClick={handleSignout}
           styleClass="mini-btn light-gray"
         />
         <Btn
