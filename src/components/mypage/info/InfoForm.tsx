27e45e5332890e4d0d6c299f1437a8a3adcef1e8
teyeong <taetae0202@naver.com>
2024-04-06 18:31:18 +0900

    fix: remove unused imports

--- src/components/mypage/info/InfoForm.tsx
+++ src/components/mypage/info/InfoForm.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from 'react';
 import Btn from 'components/_common/Btn';
 import WithdrawalModal from './WithdrawalModal';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { UserProfileAtom } from 'recoil/UserProfile';
 import { parsePhoneNumber } from 'components/utils/PhoneUtils';
 import { Signout } from 'api/user';
-import { useNavigate } from 'react-router-dom';
-import { SigninStateAtom } from 'recoil/Signin';
 
 const InfoForm = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
