af275345414591b63a652825d8b1495d01a5e85c
teyeong <taetae0202@naver.com>
2024-04-12 16:01:22 +0900

    feat: connect profile image api

--- src/api/user.ts
+++ src/api/user.ts
@@ -29,3 +29,27 @@ export const CheckUsername = async (username: string) => {
     console.log('아이디 중복 체크 실패');
   }
 };
+
+export const GetProfile = async (id: number) => {
+  try {
+    const res = await http.get(`/users/profile/${id}`);
+    console.log('프로필 사진 조회 성공', res);
+    return res;
+  } catch (err) {
+    console.log('프로필 사진 조회 실패', err);
+  }
+};
+
+export const ChangeProfile = async (id: number, form: FormData) => {
+  try {
+    const res = await http.post(`/users/profile/${id}/`, form, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    console.log('프로필 변경 성공', res);
+    return res;
+  } catch (err) {
+    console.log('프로필 변경 실패', err);
+  }
+};
