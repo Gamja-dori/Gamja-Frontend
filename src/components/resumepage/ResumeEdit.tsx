96b1f762f7d07907be37ded5da2c7f3d90f9aa8c
hanby-choi <hanby.choi@ewhain.net>
2024-04-13 23:42:57 +0900

    feat: connect submit resume api

--- src/components/resumepage/ResumeEdit.tsx
+++ src/components/resumepage/ResumeEdit.tsx
@@ -5,7 +5,7 @@ import ResumeIntro from './ResumeIntro';
 import { ResumeAtom } from 'recoil/Resume';
 import { SigninAtom } from 'recoil/Signin';
 import { useRecoilValue, useRecoilState } from 'recoil';
-import { GetResume, UpdateResume } from 'api/resume';
+import { GetResume, UpdateResume, SubmitResume } from 'api/resume';
 
 const ResumeEdit = () => {
   const navigate = useNavigate();
@@ -34,7 +34,19 @@ const ResumeEdit = () => {
 
   const updateResume = async (user_id: number, resume_id: number) => {
     const res = await UpdateResume(user_id, resume_id, resume);
-    alert('성공적으로 저장되었습니다.');
+    setResume((prev) => {
+      return {
+        ...prev,
+        is_submitted: false,
+      };
+    });
+    alert('이력서가 성공적으로 저장되었습니다.');
+  };
+
+  const submitResume = async (user_id: number, resume_id: number) => {
+    const res = await SubmitResume(user_id, resume_id);
+    alert('이력서가 성공적으로 인재풀에 등록되었습니다.');
+    navigate('/resume');
   };
 
   useEffect(() => {
@@ -77,7 +89,7 @@ const ResumeEdit = () => {
             ) : (
               <button
                 className={`resume-submit-btn ${'dark-green'}`}
-                onClick={() => navigate('/resume')}
+                onClick={() => submitResume(id, resumeId)}
               >
                 인재풀 등록
               </button>
