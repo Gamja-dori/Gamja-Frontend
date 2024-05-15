fd44c137119eb452a7c8f9dc49d9f9b27442556c
teyeong <taetae0202@naver.com>
2024-05-15 17:14:54 +0900

    fix: refactor ReviewStar

--- src/components/reviewpage/ReviewStar.tsx
+++ src/components/reviewpage/ReviewStar.tsx
@@ -3,24 +3,13 @@ import starfill from 'assets/icons/starfill.svg';
 import { ReviewStarProps } from 'props-type';
 
 const ReviewStar = ({ starRate, styleClass }: ReviewStarProps) => {
-  const rate = [];
-
-  for (let i = 0; i < starRate; i++) {
-    rate.push(true);
-  }
-  for (let i = 0; i < 5 - starRate; i++) {
-    rate.push(false);
-  }
-
   return (
     <div className={`${styleClass} review-star`}>
-      {rate.map((bool) => {
-        return (
-          <div key={null}>
-            <img src={bool ? starfill : star} />
-          </div>
-        );
-      })}
+      {[...Array(5)].map((_, index) => (
+        <div key={index}>
+          <img src={index < starRate ? starfill : star} />
+        </div>
+      ))}
     </div>
   );
 };
