e51ad23006365a727ae97af9621c1ec0e592e37e
teyeong <taetae0202@naver.com>
2024-05-20 20:57:57 +0900

    add: user_id to response

--- src/components/searchpage/ResumeDetail.tsx
+++ src/components/searchpage/ResumeDetail.tsx
@@ -23,6 +23,7 @@ const ResumeDetail = () => {
     setResumeData(() => {
       return {
         ...res?.data.resume,
+        user_id: res?.data.user_id,
         resume_id: res?.data.resume_id,
         name: res?.data.name,
         profile_image: res?.data.profile_image,
