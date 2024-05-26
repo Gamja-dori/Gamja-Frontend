760b411a31560a8a317430da71a17232bbeca466
teyeong <taetae0202@naver.com>
2024-05-26 13:01:44 +0900

    add: access verification logic to review page

--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -1,12 +1,35 @@
 import Title from 'components/_common/Title';
 import ReviewList from 'components/reviewpage/ReviewList';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { ResumeDetailAtom } from 'recoil/Recommendation';
 
 const ReviewPage = () => {
+  const seniorId = Number(useParams()['seniorId']);
+  const resumeData = useRecoilValue(ResumeDetailAtom);
+
+  const [access, setAccess] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (seniorId !== resumeData.user_id) {
+      alert('비정상적인 접근입니다.');
+      navigate(-1);
+    } else {
+      setAccess(true);
+    }
+  }, [seniorId, resumeData.user_id]);
+
   return (
-    <div className="container">
-      <Title label="리뷰" />
-      <ReviewList />
-    </div>
+    <>
+      {access && (
+        <div className="container">
+          <Title label="리뷰" />
+          <ReviewList />
+        </div>
+      )}
+    </>
   );
 };
 
